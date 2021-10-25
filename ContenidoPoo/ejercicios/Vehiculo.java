b677c44480d161bcd384da65b925db9b7c41ae09
Miguel Ramos <return.infox@gmail.com>
2021-10-25 19:03:51 -0300

    Ejercicio de Vehiculos y Herencia

--- ContenidoPoo/ejercicios/Vehiculo.java
+++ ContenidoPoo/ejercicios/Vehiculo.java
@@ -1,5 +1,32 @@
 package ejercicios;
 
 public class Vehiculo {
+	// private solo es accesible dentro de la clase (Vehiculo)
+	private String marca;
+	private String modelo;
+	private int precio;
+	
+	public Vehiculo(String marca, String modelo, int precio) {
+		// super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.precio = precio;
+	}
 
+	// por medio del metodo de acceso publico retorno el contenido de la variable private precio
+	public int getPrecio() {
+		return precio;
+	}
+    
+	//@Override
+	// public String toString() {
+	//	return "Vehiculo [marca=" + marca + ", modelo=" + modelo + ", precio=" + precio + "]";
+	//}
+	
+	public String mostrarDatos() {
+		return "Marca: " + marca + "\nModelo: " + modelo + "\nPrecio: $" + precio + "\n";
+	}
+	
+	
+	
 }
---
+++ ContenidoPoo/herencia/Animal.java
@@ -0,0 +1,20 @@
+package herencia;
+
+public class Animal {
+	
+	public String nombre;
+	// public int edad;
+	
+	public void respirar() {
+		System.out.println("Soy un animal y estoy respirando");
+	}
+	
+	public void caminar() {
+		System.out.println("Soy un animal y estoy caminado");
+	}
+	
+	public void comer() {
+		System.out.println("Soy un animal y estoy comiendo");
+	}
+
+}
